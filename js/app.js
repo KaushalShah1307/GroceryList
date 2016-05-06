b03a509b40ca460be5737ba77a1c7e95dfdebf72
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-06 15:31:12 -0400

    Added a new controller with grocerylistitems

--- js/app.js
+++ js/app.js
@@ -6,3 +6,10 @@ var app = angular.module("groceryListApp", []);
 app.controller ("appTitle", ["$scope", function ($scope) {
     $scope.title = "Grocery List";
 }]);
+
+app.controller ("groceryList", ["$scope", function () {
+    $scope.items = [
+        {completed: true, name: "milk", date: "01/01/2016"},
+        {completed: true, name: "butter", date: "02/01/2016"}
+    ];
+}]);
