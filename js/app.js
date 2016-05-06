32ffc5d508bfd07b8433affb16f17a936a92c88c
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-06 15:59:13 -0400

    Added more items to the controller list

--- js/app.js
+++ js/app.js
@@ -10,6 +10,12 @@ app.controller ("appTitle", ["$scope", function ($scope) {
 app.controller ("groceryList", ["$scope", function ($scope) {
     $scope.items = [
         {completed: true, name: "milk", date: "01/01/2016"},
-        {completed: true, name: "butter", date: "02/01/2016"}
+        {completed: true, name: "butter", date: "02/01/2016"},
+        {completed: true, name: "bread", date: "03/01/2016"},
+        {completed: true, name: "eggs", date: "04/01/2016"},
+        {completed: true, name: "sugar", date: "05/01/2016"},
+        {completed: true, name: "salt", date: "06/01/2016"},
+        {completed: true, name: "pepper", date: "07/01/2016"},
+        {completed: true, name: "cheese", date: "08/01/2016"}
     ];
 }]);
