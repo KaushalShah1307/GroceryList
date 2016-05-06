d9f01762f55bdcaa8bebec35caddb4fb32045370
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-06 18:01:21 -0400

    Added a new service listing the array of the groceryItems

--- js/app.js
+++ js/app.js
@@ -22,21 +22,31 @@ app.config (function ($routeProvider){
         })
 });
 
-app.controller ("appTitle", ["$scope", function ($scope) {
+app.service ("GroceryService", function () {
+
+        var groceryService = {};
+
+        groceryService.items = [
+            {completed: true, name: "milk", date: "01/01/2016"},
+            {completed: true, name: "butter", date: "02/01/2016"},
+            {completed: true, name: "bread", date: "03/01/2016"},
+            {completed: true, name: "eggs", date: "04/01/2016"},
+            {completed: true, name: "sugar", date: "05/01/2016"},
+            {completed: true, name: "salt", date: "06/01/2016"},
+            {completed: true, name: "pepper", date: "07/01/2016"},
+            {completed: true, name: "cheese", date: "08/01/2016"}
+        ];
+
+        return groceryService;
+});
+
+app.controller ("appTitle", ["$scope", "GroceryService",function ($scope, GroceryService) {
     $scope.title = "Grocery List";
 }]);
 
-app.controller ("groceryList", ["$scope", "$routeParams", function ($scope, $routeParams) {
-    $scope.items = [
-        {completed: true, name: "milk", date: "01/01/2016"},
-        {completed: true, name: "butter", date: "02/01/2016"},
-        {completed: true, name: "bread", date: "03/01/2016"},
-        {completed: true, name: "eggs", date: "04/01/2016"},
-        {completed: true, name: "sugar", date: "05/01/2016"},
-        {completed: true, name: "salt", date: "06/01/2016"},
-        {completed: true, name: "pepper", date: "07/01/2016"},
-        {completed: true, name: "cheese", date: "08/01/2016"}
-    ];
+app.controller ("groceryList", ["$scope", "$routeParams", "GroceryService", function ($scope, $routeParams, GroceryService) {
+
+    $scope.items = GroceryService.items;
 
         $scope.rp = "This is the parameter value: " +$routeParams.id;
 
