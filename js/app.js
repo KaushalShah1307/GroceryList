88fd0b780ecd49941f0c23cd6207c16d172f700a
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-06 18:24:13 -0400

    Defined a new ng-model to take input from the user to add new item to the list and later a service handles the saving of the added item to the groceryList

--- js/app.js
+++ js/app.js
@@ -27,15 +27,19 @@ app.service ("GroceryService", function () {
         var groceryService = {};
 
         groceryService.items = [
-            {completed: true, name: "milk", date: "01/01/2016"},
-            {completed: true, name: "butter", date: "02/01/2016"},
-            {completed: true, name: "bread", date: "03/01/2016"},
-            {completed: true, name: "eggs", date: "04/01/2016"},
-            {completed: true, name: "sugar", date: "05/01/2016"},
-            {completed: true, name: "salt", date: "06/01/2016"},
-            {completed: true, name: "pepper", date: "07/01/2016"},
-            {completed: true, name: "cheese", date: "08/01/2016"}
+            {id: 1, completed: true, name: "milk", date: "01/01/2016"},
+            {id: 2, completed: true, name: "butter", date: "02/01/2016"},
+            {id: 3, completed: true, name: "bread", date: "03/01/2016"},
+            {id: 4, completed: true, name: "eggs", date: "04/01/2016"},
+            {id: 5, completed: true, name: "sugar", date: "05/01/2016"},
+            {id: 6, completed: true, name: "salt", date: "06/01/2016"},
+            {id: 7, completed: true, name: "pepper", date: "07/01/2016"},
+            {id: 8, completed: true, name: "cheese", date: "08/01/2016"}
         ];
+
+        groceryService.save = function (entry) {
+            groceryService.items.push(entry);
+        }
 
         return groceryService;
 });
@@ -44,9 +48,15 @@ app.controller ("appTitle", ["$scope", "GroceryService",function ($scope, Grocer
     $scope.title = "Grocery List";
 }]);
 
-app.controller ("groceryList", ["$scope", "$routeParams", "GroceryService", function ($scope, $routeParams, GroceryService) {
+app.controller ("groceryList", ["$scope", "$routeParams", "GroceryService", "$location", function ($scope, $routeParams, GroceryService, $location) {
 
     $scope.items = GroceryService.items;
+    $scope.groceryItem = {id:9, completed: true, name: "tortillas", date: new Date()};
+    
+    $scope.save = function () {
+        GroceryService.save ($scope.groceryItem);
+        $location.path("/");
+    }
 
         $scope.rp = "This is the parameter value: " +$routeParams.id;
 
